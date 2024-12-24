62a0ff10d18fc0c65b44d49143e39412e39144ad
Arsa <arsa@core>
2024-12-24 21:07:58 +0700

    minor update

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+// loading screen
+window.addEventListener("load", () => {
+    const loading = document.querySelector(".loading")
+    loading.style.display = "none"
+})
+
+
+// navbar
 const bg = document.querySelectorAll(".bg");
+const sections = document.querySelectorAll("section")
 const titleHero = document.querySelector(".hero h1");
+
+const links = document.querySelectorAll(".nav-content li a")
+
+const setActive = () => {
+    let currentSection = ""
+
+    sections.forEach((e) => {
+        const sectionTop = e.offsetTop - 50
+        const sectionY = e.offsetHeight
+
+        if(window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionY){
+            currentSection = e.getAttribute("id")
+        }
+        links.forEach((link) => {
+            if(link.getAttribute("href") === `#${currentSection}`){
+                link.classList.add("active")
+            }else {
+                link.classList.remove("active")
+            }
+        })
+    })
+}
+
+window.addEventListener("scroll", setActive)
+setActive()
 
 window.addEventListener("scroll", () => {
   let top = window.scrollY;
@@ -15,4 +49,14 @@ window.addEventListener("scroll", () => {
   });
 
   titleHero.style.transform = `translateY(${top / 2}px)`;
+
+//   sections.forEach((e) => {
+//     if(e.offsetTop - top < 550){
+//         e.classList.add("active")
+//     }else {
+//         e.classList.remove("active")
+//     }
+//   })
 });
+
+
